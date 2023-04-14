f8a42d245d353959ebd2320a63640b59c72495a1
Crying-Goose <fkauswlq789@gmail.com>
2023-04-15 01:59:47 +0900

    init project

---
+++ Glo/Glo/GloApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloApp.swift
+//  Glo
+//
+//  Created by 라주영 on 2023/04/15.
+//
+
+import SwiftUI
+
+@main
+struct GloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
