4604d61fe143c36e29d91b29b40701ca41ee4412
Crying-Goose <fkauswlq789@gmail.com>
2023-05-19 14:31:26 +0900

    TCA 추가

---
+++ Glo/Glo/Views/ArticleView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleView.swift
+//  Glo
+//
+//  Created by 라주영 on 2023/05/19.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
---
+++ Glo/Glo/Views/WritingView.swift
@@ -0,0 +1,20 @@
+//
+//  WritingView.swift
+//  Glo
+//
+//  Created by 라주영 on 2023/05/19.
+//
+
+import SwiftUI
+
+struct WritingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WritingView_Previews: PreviewProvider {
+    static var previews: some View {
+        WritingView()
+    }
+}
